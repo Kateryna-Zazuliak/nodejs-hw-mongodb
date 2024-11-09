ae08f9437d4a5f6e048edffd2976c21db8daa28b
Kateryna-Zazuliak <kateryna_didenko@ukr.net>
2024-11-10 00:04:33 +0200

    add auth docs

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -20,10 +20,5 @@ const userSchema = new Schema(
     versionKey: false,
   },
 );
-// userSchema.methods.toJSON = function () {
-//   const obj = this.toObject();
-//   delete obj.password;
-//   return obj;
-// };
 
 export const UsersCollection = model('user', userSchema);
--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,10 +1,10 @@
-import createHttpErrors from 'http-errors';
+import createHttpError from 'http-errors';
 
 export const validateBody = (schema) => async (req, res, next) => {
   try {
-    await schema.validateAsync(req.body, { abortEarly: true });
+    await schema.validateAsync(req.body, { abortEarly: false });
     next();
   } catch (e) {
-    next(createHttpErrors(401, 'Bad request', { errors: e.details }));
+    next(createHttpError(400, 'Bad request', { errors: e.details }));
   }
 };
